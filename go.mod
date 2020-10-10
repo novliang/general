07477acf4be16989150253a7918134ceff94da0e
lion0418@163.com <lion0418@163.com>
2020-10-10 15:16:10 +0800

    add response
    Signed-off-by: lion0418@163.com <lion0418@163.com>

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/novliang/general
+
+go 1.14
---
+++ response/response.go
@@ -0,0 +1,7 @@
+package response
+
+type Response struct {
+	Code    int         `json:"Code"`
+	Data    interface{} `json:"Data"`
+	Message string      `json:"Message"`
+}
